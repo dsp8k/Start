fec66a5d5fbcb83663ca045c4d1339c49170e900
dsp8k <princewilldike9@gmail.com>
2025-01-30 10:49:39 +0100

    gabage collection

---
+++ Start/Overview/MyConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+string response;
+console.writeline("what's your name?");
+response = console.readline();
+console.writeline($"enjoy the curse, (response)");
+
+opertaingsystem thisOS = environment.OSVersion;
+console.writeline(thisOS.playform);
+console.writeline(thisOS.versionstring);
