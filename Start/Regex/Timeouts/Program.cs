cb93faf362799a28ce5e55db7bb38645405db912
dsp8k <princewilldike9@gmail.com>
2025-02-20 10:25:53 +0100

    timeouts

--- Start/Regex/Timeouts/Program.cs
+++ Start/Regex/Timeouts/Program.cs
@@ -3,18 +3,19 @@
 using System.Text.RegularExpressions;
 using System.Diagnostics;
 
-const string thestr = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+const int MAX_REGEX_TIME = 1000; //timeout value in milliseconds
+const string thestr = "aaaaaaaaaaaaaaab";
 
 // Use a Stopwatch to show elapsed time
 Stopwatch sw;
 
 // TODO: Use a Timeout value when executing RegEx to guard against bad input
-
+TimeSpan Timeout = TimeSpan.FromMilliseconds(MAX_REGEX_TIME);
 
 // Run the expression and output the result
 try {
     sw = Stopwatch.StartNew();
-    Regex CapWords = new Regex(@"(a+a+)+b", RegexOptions.None);
+    Regex CapWords = new Regex(@"(a+a+)+b", RegexOptions.None, Timeout);
     MatchCollection mc = CapWords.Matches(thestr);
     sw.Stop();
     Console.WriteLine($"Found {mc.Count} matches in {sw.Elapsed} time:");
@@ -25,3 +26,5 @@ try {
 catch (Exception e) {
     Console.WriteLine(e);
 }
+
+
